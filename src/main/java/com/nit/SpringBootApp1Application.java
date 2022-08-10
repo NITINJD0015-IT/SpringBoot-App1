dd8377868e42b5dcb1e256ed2aefb60d43694d37
nitin <nitindeshmukhit@gmail.com>
2022-08-10 19:50:32 +0530

    Second commit

--- src/main/java/com/nit/SpringBootApp1Application.java
+++ src/main/java/com/nit/SpringBootApp1Application.java
@@ -8,6 +8,7 @@ public class SpringBootApp1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApp1Application.class, args);
+		System.out.println("Hello");
 	}
 
 }
